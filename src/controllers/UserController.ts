55d0813973f207ad8a8862237e50058f35b419c9
Thanapoom.C <palmdz08@gmail.com>
2025-02-16 16:51:39 +0700

    Create: user, course, enrollment, review schema

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,32 +3,32 @@ import UserRepository from "../repositories/UserRepository";
 import { User } from "@prisma/client";
 
 const userController = new Elysia({
-    prefix: "/api/user",
-    tags: ["User"]
-})
+  prefix: "/api/user",
+  tags: ["User"],
+});
 
 userController.model({
-    uuid: t.String(),
-    username: t.String(),
-    email: t.String(),
-    password: t.String(),
-    name: t.String(),
-    surname: t.String()
-})
+  uuid: t.String(),
+  username: t.String(),
+  email: t.String(),
+  password: t.String(),
+  name: t.String(),
+  surname: t.String(),
+});
 
 userController.get(
-    "/all",
-    async () => {
-        const userRepository = new UserRepository();
-        const users: User[]  = await userRepository.getAllUsers();
-        return users;
+  "/all",
+  async () => {
+    const userRepository = new UserRepository();
+    const users: User[] = await userRepository.getAllUsers();
+    return users;
+  },
+  {
+    detail: {
+      summary: "Get All User", //API Name
+      description: "Get all user from database", //API Description
     },
-    {
-        detail: {
-            summary: "Get All User", //API Name
-            description: "Get all user from database" //API Description
-        }
-    }
+  }
 );
 
 export default userController;
